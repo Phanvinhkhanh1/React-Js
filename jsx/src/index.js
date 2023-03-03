9ebbffb01183fa429ede06e9222eed08278d72db
Phan Vinh Khanh <Khanh161297@gmail.com>
2023-03-03 22:39:09 +0700

    Print JS variables in JSX

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -6,7 +6,13 @@ const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
 function App() {
-    return <h1>Hi Phan Vinh Khanh</h1>;
+    let message = "Hi Phan Vinh Khanh";
+
+    if (Math.random() > 0) {
+        message = "Hello your visa : PANK";
+    }
+
+    return <h1>{message}</h1>;
 }
 
 root.render(<App />);
