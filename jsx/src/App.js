b4420f1b64fa1dcb118ee66d202b7149b1cf613c
Phan Vinh Khanh <Khanh161297@gmail.com>
2023-03-04 11:16:39 +0700

    Extracting-Components

---
+++ jsx/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+    return <textarea autoFocus={true} style={{ border: "solid 1px red", background: "green" }} />
+}
+
+export default App;
--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,12 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './App';
 
 const el = document.getElementById('root');
 
 const root = ReactDOM.createRoot(el);
-
-function App() {
-    return <textarea autoFocus={true} style={{border : "solid 1px red",background : "green"}}/>
-}
 
 root.render(<App />);
