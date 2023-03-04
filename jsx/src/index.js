1173c665554d60a535165800a37ec5dd5ee74678
Phan Vinh Khanh <Khanh161297@gmail.com>
2023-03-04 11:00:46 +0700

    Convert-HTML-to-JSX

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -6,7 +6,7 @@ const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
 function App() {
-    return <input type="number" min={5} max={10} style={{border : '1px solid red', background : 'green'}}/>
+    return <textarea autoFocus={true} style={{border : "solid 1px red",background : "green"}}/>
 }
 
 root.render(<App />);
